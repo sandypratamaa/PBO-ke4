254ed59c9ae9c6497d7d9d91b515b82004f18502
sandypratamaa <102770747+sandypratamaa@users.noreply.github.com>
2022-04-03 19:17:36 +0700

    Create motherboardgo.java

---
+++ motherboardgo.java
@@ -0,0 +1,14 @@
+package pbogab;
+import java.util.Scanner;
+public class MotherBoardGo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		Motherboard.USB usb = new Motherboard.USB();
+		usb.usb2 = input.nextInt();
+		usb.usb3 = input.nextInt();
+		System.out.println("Total Ports = "+usb.getTotalPorts());
+	}
+}
+© 2022 GitH
